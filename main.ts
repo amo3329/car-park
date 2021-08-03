d12f4f27772ed783dafa19b1cb6a805185e07e14
amo3329 <86691133+amo3329@users.noreply.github.com>
2021-08-04 09:49:12 +1200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,18 +6,30 @@ radio.onReceivedString(function (receivedString) {
         led.plot(1, 1)
         led.plot(1, 2)
         led.plot(1, 3)
+        led.plot(0, 1)
+        led.plot(0, 2)
+        led.plot(0, 3)
     } else if ("P1EMPTY".compare(receivedString) == 0) {
         led.unplot(1, 1)
         led.unplot(1, 2)
         led.unplot(1, 3)
+        led.unplot(0, 1)
+        led.unplot(0, 2)
+        led.unplot(0, 3)
     } else if ("P2FULL".compare(receivedString) == 0) {
         led.plot(3, 1)
         led.plot(3, 2)
         led.plot(3, 3)
+        led.plot(4, 1)
+        led.plot(4, 2)
+        led.plot(4, 3)
     } else if ("P2EMPTY".compare(receivedString) == 0) {
         led.unplot(3, 1)
         led.unplot(3, 2)
         led.unplot(3, 3)
+        led.unplot(4, 1)
+        led.unplot(4, 2)
+        led.unplot(4, 3)
     }
 })
 input.onButtonPressed(Button.B, function () {
